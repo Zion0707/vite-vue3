1cb520ef1c09258892735e54e9f75ee04a6d743c
huangzihao <346445684@qq.com>
2022-10-31 09:41:03 +0800

    项目初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <p>{{ msg }}</p>
+  <button type="button" @click="count++">{{ count }}</button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/routers/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const routes = [
+  { path: "/", redirect: "/home" },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../pages/login/index.vue"),
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: () => import("../pages/home/index.vue"),
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () => import("../pages/about/index.vue"),
+  },
+  {
+    path: "/404",
+    name: "404",
+    component: () => import("../pages/no-found/index.vue"),
+  },
+  {
+    path: "/:w+",
+    name: "*",
+    redirect: "/404",
+  },
+];
+
+export const routers = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
